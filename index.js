1d90fdb4891e1443e0c5e5340ebcdbf4c6f23a47
Oganesman <96392987+qweRTyiOp123789@users.noreply.github.com>
2022-04-26 14:36:03 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 module.exports = async function (waw) {
 	const stripe = require('stripe')(waw.config.stripe);
-	const router = waw.router("/api/stripe")
-	router.post("/source", (req, res) => {
-		stripe.charges.create({
+	const router = waw.router("/api/stripe");
+	router.post("/charge", async (req, res) => {
+		const change = await stripe.charges.create({
 			amount: req.body.amount,
 			currency: 'usd',
 			source: req.body.source.id,
 			description: 'My First Test Charge (created for API docs)',
 		});
-		res.json(true);
-	})
+		res.json(change.paid);
+	});
+	router.post("/subscribe", async (req, res) => {
+		const change = await stripe.charges.create({
+			amount: req.body.amount,
+			currency: 'usd',
+			source: req.body.source.id,
+			description: 'My First Test Charge (created for API docs)',
+		});
+		res.json(change.paid);
+	});
 };
